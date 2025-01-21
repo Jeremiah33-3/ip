adc67aee568b19747ad43987154a82d4d7e870de
WeiTianyue <e0968931@u.nus.edu>
2025-01-21 16:09:17 +0800

    Add the functionality to add to-do task to a list and then list it.

--- src/main/java/HeyJudy.java
+++ src/main/java/HeyJudy.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
 
 public class HeyJudy {
+    private static final String[] tasks = new String[100];
+    private static int count = 0;
     public static void greetUser() {
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm HeyJudy");
+        System.out.println(" Hello! I'm HeyJudy.");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
     }
-    public static void echoCommand(String command) {
+    public static void addItem(String item) {
+        tasks[count] = item;
+        count++;
         System.out.println("    ____________________________________________________________");
-        System.out.println("        " + command);
+        System.out.println("        added: " + item);
         System.out.println("    ____________________________________________________________");
     }
     public static void exit() {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("         Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+    public static void printTask() {
+        if (count == 0) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("        Psss, I don't see any task yet. Please add");
+            System.out.println("    ____________________________________________________________");
+        } else {
+            System.out.println("    ____________________________________________________________");
+            for (int i = 0; i < count; i++) {
+                System.out.println("        " + (i + 1) + ". " + tasks[i]);
+            }
+            System.out.println("    ____________________________________________________________");
+        }
     }
     public static void readCommand() {
         Scanner scanner = new Scanner(System.in);
@@ -26,10 +43,13 @@ public class HeyJudy {
             if (userCommand.equalsIgnoreCase("bye")) {
                 HeyJudy.exit();
                 break;
+            } else if (userCommand.equalsIgnoreCase("list")) {
+                HeyJudy.printTask();
+            } else {
+                HeyJudy.addItem(userCommand);
             }
-            HeyJudy.echoCommand(userCommand);
         }
-        
+
         scanner.close();
     }
     public static void main(String[] args) {
